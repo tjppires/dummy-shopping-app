6f3706766d6c56d39f19688be2d80d047fe4970e
Telmo Pires <telmo@unbabel.com>
2018-04-04 23:14:13 +0100

    Updated to use qrin technology

--- melons.py
+++ melons.py
@@ -1,7 +1,69 @@
-from flask import Flask, request, session, render_template, g, redirect, url_for, flash
+from flask import Flask, request, session, render_template, g, redirect, url_for, flash, jsonify
 import model
 import jinja2
 import os
+import qrcode
+import uuid
+import threading
+import time
+
+TIMEOUT = 40  # ~40 seconds (poorly implemented, can be less - at least 30s)
+SLEEP_TIME = 10  # 10 seconds
+jobs_lock = threading.Lock()
+job_stats = {}
+STORE_NAME = "MyStoreQR"
+
+class Job:
+    def __init__(self, uid, price):
+        self.uid = uid
+        self.price = price
+        self.status = "waiting"
+        self.ttl = TIMEOUT
+        self.store_name = STORE_NAME
+    def timeout(self):
+        self.status = "timeout"
+        self.ttl = 0
+    def complete(self):
+        if self.status != "timeout":
+            self.status = "completed"
+    def is_completed(self):
+        return self.status == "completed"
+    def __repr__(self):
+        return "{0}:{1}".format(self.status, self.ttl)
+
+def manage_timeouts():
+    while True:
+        time.sleep(SLEEP_TIME)
+        jobs_lock.acquire()
+        for uid in job_stats.keys():
+            job_stats[uid].ttl -= SLEEP_TIME
+            if job_stats[uid].ttl <= 0 and not job_stats[uid].is_completed():
+                job_stats[uid].timeout()
+        jobs_lock.release()
+
+t = threading.Thread(target=manage_timeouts)
+t.daemon = True
+t.start()
+
+def add_job(uid, price):
+    jobs_lock.acquire()
+    job_stats[uid] = Job(uid, price)
+    jobs_lock.release()
+
+def check_job(uid):
+    if uid is None:
+        return None
+    jobs_lock.acquire()
+    result = job_stats.get(uid)
+    jobs_lock.release()
+    return result
+
+def complete_job(uid):
+    if uid is None:
+        return
+    jobs_lock.acquire()
+    job_stats[uid].complete()
+    jobs_lock.release()
 
 app = Flask(__name__)
 app.secret_key = '\xf5!\x07!qj\xa4\x08\xc6\xf8\n\x8a\x95m\xe2\x04g\xbb\x98|U\xa2f\x03'
@@ -10,6 +72,7 @@ app.jinja_env.undefined = jinja2.StrictUndefined
 @app.route("/")
 def index():
     """This is the 'cover' page of the ubermelon site"""
+    return redirect("/melons")
     return render_template("index.html")
 
 @app.route("/melons")
@@ -98,12 +161,63 @@ def process_login():
         return render_template("login.html")
 
 
+@app.route("/empty")
+def empty_chart():
+    """Empty chart"""
+    session.clear()
+    return redirect("/melons")
+
+
 @app.route("/checkout")
 def checkout():
-    """TODO: Implement a payment system. For now, just return them to the main
-    melon listing page."""
-    flash("Sorry! Checkout will be implemented in a future version of ubermelon.")
-    return redirect("/melons")
+    """QR checkout system"""
+    flash("1-Click QR code checkout")
+    if "cart" not in session:
+        flash("There is nothing in your cart.")
+        return redirect("/melons")
+    else:
+        items = session["cart"]
+        dict_of_melons = {}
+
+        total_price = 0
+        for item in items:
+            melon = model.get_melon_by_id(item)
+            total_price += melon.price
+    uid = uuid.uuid4().hex
+    qr_img = qrcode.make(uid)
+    qr_path = "static/img/{0}.png".format(uid)
+    qr_img.save(qr_path)
+    add_job(uid, total_price)
+    return render_template("checkout.html", display_cart={}, total=total_price, qr_id=uid)
+
+@app.route("/success")
+def success():
+    session.clear()
+    return render_template("success.html")
+
+@app.route("/timeout")
+def timeout():
+    return render_template("transaction_failed.html")
+
+@app.route("/validate", methods=['GET', 'POST'])
+def qr_validation():
+    """REST API for QR validation"""
+    if request.method == "GET":
+        uid = request.args.get("uid")
+        job = check_job(uid)
+        if not job is None:
+            response = {"status": job.status, "price": job.price, "store": job.store_name}
+        else:
+            response = {"status": "invalid_id"}
+    elif request.method == "POST":
+        uid = request.values.get("uid")
+        complete_job(uid)
+        if uid is None:
+            response = {"status": "invalid_id"}
+        else:
+            response = {"status": "completed"}
+    return jsonify(response)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
